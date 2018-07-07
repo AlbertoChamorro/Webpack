7b178c09ba0770d9a902eb3fa55b578cf57ebb81
Alberto Chamorro <albertchc04@gmail.com>
2018-07-07 13:53:52 -0600

    refactor webpack, files common

---
+++ refactor-webpack/dist/js/contact.js
@@ -0,0 +1,47 @@
+webpackJsonp([1],{
+
+/***/ 34:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(6);
+
+var _teachers = __webpack_require__(7);
+
+var _teachers2 = _interopRequireDefault(_teachers);
+
+var _renderDom = __webpack_require__(8);
+
+var _renderDom2 = _interopRequireDefault(_renderDom);
+
+var _builderDom = __webpack_require__(9);
+
+var _builderDom2 = _interopRequireDefault(_builderDom);
+
+var _react = __webpack_require__(1);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = __webpack_require__(12);
+
+var _teachers3 = __webpack_require__(17);
+
+var _teachers4 = _interopRequireDefault(_teachers3);
+
+__webpack_require__(18);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _reactDom.render)(_react2.default.createElement(_teachers4.default, { data: _teachers2.default }), document.getElementById('container'));
+
+// data.teachers.forEach(teacher => {
+//     console.log(teacher)
+//     const item = generator.makeItemList('li', teacher.name)
+//     renderToDOM.render(item)    
+// })
+
+/***/ })
+
+},[34]);
---
+++ refactor-webpack/src/js/render-dom.js
@@ -0,0 +1,3 @@
+exports.render = (element) => {
+    document.body.append(element)
+}
